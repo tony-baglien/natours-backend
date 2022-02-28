ec54ee7f4b577c9e9a29a85c021cbec82da83fb1
Tony B <tony.b.composition@gmail.com>
2022-02-28 14:24:14 -0600

    finished lesson 99

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -19,10 +19,30 @@ exports.getAllTours = async (req, res) => {
 
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
+    }
+
+    // 3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4) Pagination
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     //EXECUTE QUERY
