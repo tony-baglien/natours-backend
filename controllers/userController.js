12d72bd5cc9ccbddf8ea2f79d1605d5d8ddf27c4
Tony Baglien <tbaglien@crescendocollective.com>
2022-03-03 22:37:33 -0600

    finished lessons on aggrigation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,32 +1,30 @@
-
-
-exports.getAllUsers = (req,res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet implimented'
-    })
-}
-exports.createUser = (req,res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet implimented'
-    })
-}
-exports.getUser = (req,res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet implimented'
-    })
-}
-exports.updateUser = (req,res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet implimented'
-    })
-}
-exports.deleteUser = (req,res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet implimented'
-    })
-}
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet implimented',
+  });
+};
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet implimented',
+  });
+};
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet implimented',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet implimented',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet implimented',
+  });
+};
