a0973cb164157ce5495c5d495946864421eeadf7
Tony Baglien <tbaglien@crescendocollective.com>
2022-03-13 00:12:08 -0600

    TODO: figure out why prod is  not working

--- app.js
+++ app.js
@@ -26,11 +26,6 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server!`,
-  // });
-
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
--- server.js
+++ server.js
@@ -25,4 +25,5 @@ mongoose
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running  on port ${port}...`);
+  console.log(process.env);
 });
